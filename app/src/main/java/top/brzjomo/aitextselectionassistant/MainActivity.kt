12a02fae0d50fb930c522afd709dfa08a254bd1d
Brzjomo <zhiqiang_xue@outlook.com>
2026-01-08 17:00:56 +0800

    修复暗色模式页面切换闪烁

--- app/src/main/java/top/brzjomo/aitextselectionassistant/MainActivity.kt
+++ app/src/main/java/top/brzjomo/aitextselectionassistant/MainActivity.kt
@@ -11,11 +11,16 @@ import top.brzjomo.aitextselectionassistant.ui.main.MainScreen
 import top.brzjomo.aitextselectionassistant.ui.theme.AITextSelectionAssistantTheme
 import top.brzjomo.aitextselectionassistant.AppContainer
 import top.brzjomo.aitextselectionassistant.LocalAppContainer
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // 设置窗口背景为透明，避免页面切换时的白色闪烁
+        window.setBackgroundDrawableResource(android.R.color.transparent)
 
         // 创建 AppContainer
         val appContainer = AppContainer(this)
